b4bdbddcf58eeb6db5833242cdb93cbdeb9d6012
troels <fqx851@alumni.ku.dk>
2020-12-22 13:05:42 +0100

    Organize as pip package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="receiptapi",
+    version="0.1",
+    description="RESTful web api for storing receipts",
+    url="https://github.com/troelsfrostholm/receipt-api",
+    author="Troels Frostholm Mogensen",
+    author_email="troelsfrostholm@gmail.com",
+    license="GPLv3.0",
+    packages=["receiptapi"],
+    zip_safe=False,
+)
