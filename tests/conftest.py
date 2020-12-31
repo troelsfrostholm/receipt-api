c881df8e7ee380899e57fce4c8a24060fc3fbbf8
troels <fqx851@alumni.ku.dk>
2020-12-31 15:27:44 +0100

    Send server output to log files during tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,19 @@
 from receiptapi import server
 from multiprocessing import Process
 import time
+import sys
 
 """ Name of test database """
 dbname = "receipts_test"
 
-serverProcess = Process(target=server.run, args=(dbname,))
+
+def run_server():
+    sys.stdout = open("server-test.log", "w")
+    sys.stderr = open("server-test-err.log", "w")
+    server.run(dbname)
+
+
+serverProcess = Process(target=run_server)
 
 
 def pytest_sessionstart(session):
